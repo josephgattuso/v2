415f9af4a9a89a91fea26dc9fa18ced60659ba7a
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-21 10:38:09 +0000

    update uses content

--- content/uses.js
+++ content/uses.js
@@ -1,18 +1,97 @@
 export const uses = [
   {
-    name: 'Essentials',
-    description:
-      'These are the things that I have with me if I am going outside. Obviously not going out that often these days due to the COVID-19 pandemic.',
+    name: 'Hardware',
+    description: '',
     sort: true,
     itemList: [
       {
-        name: 'Muji Umbrella',
+        name: '2016 MacBook Pro 13"',
+      },
+      {
+        name: 'Apple Magic Keyboard',
+      },
+      {
+        name: 'Apple Magic TrackPad 2',
+      },
+      {
+        name: 'Rain Design mStand Laptop Stand',
+      },
+    ],
+  },
+  {
+    name: 'Software',
+    description: '',
+    sort: true,
+    itemList: [
+      {
+        name: 'VS Code',
+      },
+    ],
+  },
+  {
+    name: 'Web Development',
+    description: '',
+    sort: true,
+    itemList: [
+      {
+        name: 'Libraries & Frameworks',
         attributeList: [
           {
-            name: 'Check out other brollies I like',
-            url: 'https://google.com',
+            name: 'React',
+            description: 'A library for building user interfaces written in JavaScript',
+            url: 'https://reactjs.org',
+          },
+          {
+            name: 'Redux',
+            description: 'A predictable state container for JavaScript apps',
+            url: 'https://redux.js.org/',
+          },
+          {
+            name: 'Gatsby',
+            description:
+              'An open-source framework for creating static websites and apps written in React',
+            url: 'https://gatsbyjs.org/',
+          },
+          {
+            name: 'Next.js',
+            description:
+              'hybrid static & server rendering open-source framework for creating websites and apps written in React',
+            url: 'https://nextjs.org/',
+          },
+          {
+            name: 'Bootstrap',
+            description:
+              'front-end toolkit featuring Sass variables and mixins, responsive grid system,  prebuilt components, and plugins',
+            url: 'https://getbootstrap.com/',
           },
         ],
+      },
+    ],
+  },
+  {
+    name: 'Learning',
+    description: 'General resources for learning web development:',
+    itemList: [
+      {
+        name: 'freeCodeCamp',
+        description:
+          'A free site for learning web development. The curriculum features a number of JavaScript algorithms for coding interview prep.',
+        url: 'https://freecodecamp.org',
+      },
+      {
+        name: 'Stack Overflow',
+        description: 'Massive resource of questions and answers related to coding.',
+        url: 'https://stackoverflow.com/',
+      },
+      {
+        name: 'Udemy',
+        description: 'A website for learning different concepts of computer science.',
+        url: 'https://udemy.com',
+      },
+      {
+        name: 'CSS-Tricks',
+        description: 'One of the best sites to learn CSS and responsive design.',
+        url: 'https://css-tricks.com/',
       },
     ],
   },
--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Layout from '../components/Layout';
 import { uses } from '../../content/uses';
 
 const ItemList = ({ items, parentIndex }) => (
   <ul>
     {items.map((item, itemIndex) => (
-      <React.Fragment key={itemIndex}>
+      <Fragment key={itemIndex}>
         <li key={`parent${parentIndex}-item${itemIndex}`}>
           {item.url ? <a href={item.url}>{item.name}</a> : item.name}
+          {item.description ? `: ${item.description}` : item.description}
           {item.attributeList && (
             <ul>
               {item.attributeList.map((attribute, attributeIndex) => (
                 <li key={`parent${parentIndex}-item${itemIndex}-attribute${attributeIndex}`}>
                   {attribute.url ? <a href={attribute.url}>{attribute.name}</a> : attribute.name}
+                  {attribute.description ? `: ${attribute.description}` : attribute.description}
                 </li>
               ))}
             </ul>
           )}
         </li>
-      </React.Fragment>
+      </Fragment>
     ))}
   </ul>
 );
@@ -40,23 +42,25 @@ const UsesPage = () => {
       <div className="wrapper">
         <div className="content-container with-padding">
           <p>
-            From casual web browsing to creating content for my Blog, here's a comprehensive list of
-            hardware and software that I use on a daily basis to create and consume content.
+            Here's a comprehensive list of hardware and software that I use on a daily basis to
+            create and consume content.
           </p>
-          <p>I enjoy trying out new things so this list will be updated accordingly.</p>
+          <p>I enjoy trying out new things and this list will be updated accordingly.</p>
           {uses.map((section, sectionIndex) => (
-            <React.Fragment key={sectionIndex}>
+            <Fragment key={sectionIndex}>
               <h2 className="section">{section.name}</h2>
               <p>{section.description}</p>
               {section.categoryList &&
                 section.categoryList.map((category, categoryIndex) => (
-                  <React.Fragment key={`category${categoryIndex}`}>
+                  <Fragment key={`category${categoryIndex}`}>
                     <h3>{category.name}</h3>
                     <ItemList
                       items={category.sort ? sortList(category.list) : category.list}
                       parentIndex={sectionIndex}
-                    />
-                  </React.Fragment>
+                    >
+                      <p>{section.description}</p>
+                    </ItemList>
+                  </Fragment>
                 ))}
               {section.itemList && (
                 <ItemList
@@ -64,7 +68,7 @@ const UsesPage = () => {
                   parentIndex={sectionIndex}
                 />
               )}
-            </React.Fragment>
+            </Fragment>
           ))}
         </div>
       </div>
