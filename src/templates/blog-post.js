7592661f43472fb7f0ded536f9eaef4091a4f844
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-30 22:51:59 +0000

    refactor blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,36 +4,87 @@ import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import Layout from '../components/layout/layout.component';
-import Img from '../components/image/img.component';
+// import Img from '../components/image/img.component';
+import Img from 'gatsby-image';
 
 import './blog-post.styles.scss';
+
+import {
+  ArticleContainer,
+  ArticleHeader,
+  ArticleDate,
+  ArticleH2,
+  ArticleH3,
+  ArticleH4,
+  ArticleH5,
+  ArticleH6,
+  ArticlePara,
+  ArticleBlockQuote,
+  ArticleA,
+  ArticleEm,
+  ArticleLi,
+  ArticleOl,
+  ArticleStrong,
+  ArticleUl,
+} from '../styles/PostStyles';
 
 const shortcodes = { Link }; // Provide common components here
 
 export default ({ data, pageContext }) => {
   const { frontmatter, body } = data.mdx;
   const { previous, next } = pageContext;
+  const featuredImgFluid = frontmatter.featuredImage
+    ? frontmatter.featuredImage.childImageSharp.fluid
+    : null;
+
   return (
     <Layout page={frontmatter.title} description={frontmatter.description} isBlogPost>
       <div className="wrapper">
-        <article className="content-container blog-post with-padding">
-          <div className="heading">
-            <h1 className="post-title">{frontmatter.title}</h1>
-            <p clsasName="post-subtitle">{frontmatter.date}</p>
+        <ArticleContainer className="content-container blog-post with-padding">
+          <ArticleHeader>{frontmatter.title}</ArticleHeader>
+          <ArticleDate>{frontmatter.date}</ArticleDate>
+          <Img fluid={featuredImgFluid} />
+          <div className="markdown">
+            <MDXProvider
+              components={{
+                h1: ArticleH2, // H1 reserved for article semantic title
+                h2: ArticleH2,
+                h3: ArticleH3,
+                h4: ArticleH4,
+                h5: ArticleH5,
+                h6: ArticleH6,
+                p: ArticlePara,
+                blockquote: ArticleBlockQuote,
+                ul: ArticleUl,
+                ol: ArticleOl,
+                li: ArticleLi,
+                strong: ArticleStrong,
+                em: ArticleEm,
+                a: ArticleA,
+              }}
+            >
+              <MDXRenderer className="post-body">{body}</MDXRenderer>
+            </MDXProvider>
           </div>
-          <Img image={frontmatter.image} alt={frontmatter.title} />
-          <MDXProvider components={shortcodes}>
-            <MDXRenderer className="post-body">{body}</MDXRenderer>
-          </MDXProvider>
-        </article>
-        {previous === false ? null : (
-          <>
-            {previous && <Link to={previous.fields.slug}>&larr; {previous.frontmatter.title}</Link>}
-          </>
-        )}
-        {next === false ? null : (
-          <>{next && <Link to={next.fields.slug}>{next.frontmatter.title} &rarr;</Link>}</>
-        )}
+          {previous === false ? null : (
+            <>
+              {previous && (
+                <Link to={previous.fields.slug}>
+                  <p>{previous.frontmatter.title}</p>
+                </Link>
+              )}
+            </>
+          )}
+          {next === false ? null : (
+            <>
+              {next && (
+                <Link to={next.fields.slug}>
+                  <p>{next.frontmatter.title}</p>
+                </Link>
+              )}
+            </>
+          )}
+        </ArticleContainer>
       </div>
     </Layout>
   );
@@ -43,13 +94,15 @@ export const query = graphql`
   query PostsBySlug($slug: String!) {
     mdx(fields: { slug: { eq: $slug } }) {
       body
+      excerpt(pruneLength: 160)
       frontmatter {
         title
-        date(formatString: "YYYY MMMM Do")
-        image {
+        description
+        date(formatString: "MMMM Do, YYYY")
+        featuredImage {
           childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid_withWebp
+            fluid(maxWidth: 960) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
           }
         }
